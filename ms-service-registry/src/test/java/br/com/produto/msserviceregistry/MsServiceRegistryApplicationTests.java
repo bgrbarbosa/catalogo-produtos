150a8f06b674704af929f58fda6fc7ef9dd26176
Bruno Gaspar <bgrbarbosa@hotmail.com>
2023-06-01 12:21:35 -0300

    Microservico de Service Registry criado

---
+++ ms-service-registry/src/test/java/br/com/produto/msserviceregistry/MsServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.produto.msserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
